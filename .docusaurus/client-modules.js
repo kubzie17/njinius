d7006c55e72e39aea4e6814a9ce854a3cf967430
kubzie <mckubzie@btinternet.com>
2020-10-30 03:46:18 +0000

    removal of docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Development\\Njinius\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Development\\Njinius\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Development\\Njinius\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Development\\Njinius\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Development\\Njinius\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
-  require("C:\\Development\\Njinius\\src\\css\\custom.css"),
-];
